d8e2145db9c744dd55bacc6d7ba30ddc0c3fb63f
dwmorrell <davismorrell@gmail.com>
2022-12-31 13:33:12 -0700

    Updated link

--- src/components/HeroImg.js
+++ src/components/HeroImg.js
@@ -13,7 +13,7 @@ const HeroImg = () => {
             <p>HI, MY NAME IS DAVIS</p>
             <h1>Full-Stack Developer</h1>
             <div>
-                <Link to="/project" className="btn">Projects </Link>
+                <Link to="/projects" className="btn">Projects </Link>
                 <Link to="/contact" className="btn btn-light">Contact </Link>
             </div>
         </div>
